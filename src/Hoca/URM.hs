9a41c9d636539ec50c6c28af7c6abc0d72249580
Martin Avanzini <martin.avanzini@uibk.ac.at>
2015-02-14 22:06:57 +0100

    added Usable Replacement Maps

---
+++ src/Hoca/URM.hs
@@ -0,0 +1,74 @@
+-- | 
+
+module Hoca.URM (
+  URM
+  , empty
+  , usable
+  , fromRules
+  , urm
+  , filterTerm
+  ) where
+
+import qualified Data.Map as Map
+import Data.Map (Map)
+import Data.Maybe (fromMaybe)
+import qualified Data.Rewriting.Term as T
+import qualified Data.Rewriting.Rules as RS
+import Hoca.ATRS (aterm, AView (..), Rule, Term, var, fun, app)
+import qualified Text.PrettyPrint.ANSI.Leijen as PP
+import Control.Monad.Writer (execWriter, tell)
+
+newtype URM f = URM (Map f [Int])
+
+empty :: URM f
+empty = URM Map.empty
+
+usable :: Ord f => URM f -> f -> [Int]
+usable (URM m) f = fromMaybe [] (Map.lookup f m)
+
+insert :: Ord f => URM f -> f -> Int -> URM f
+insert (URM m) f i = URM (Map.alter ins f m) where
+  ins Nothing = Just [i]
+  ins (Just us) = Just (add us)
+  add [] = [i]
+  add us@(j:js)
+    | i < j     = i : us
+    | i == j    = us
+    | otherwise = j : add js
+
+fromList :: Ord f => [(f,Int)] -> URM f
+fromList = foldl (uncurry . insert) empty
+
+urm :: Ord f => [Rule f v] -> [Term f v] -> URM f
+urm rs calls = fromList (execWriter (mapM upos calls)) where
+  upos (aterm -> t1 :@ t2) = upos t1 >> upos t2 >> return True
+  upos (aterm -> Fun f ts) = do
+    reds <- mapM upos ts
+    tell [(f,i) | (i,True) <- zip [0..] reds]
+    return (or reds || f `elem` ds)
+  upos _ = return False
+  ds = [f | Fun f _ <- map aterm (RS.lhss rs)]
+
+-- ^ approximation of usable replacement maps without unification, looking at roots only
+fromRules :: Ord f => [Rule f v] -> URM f
+fromRules rs = urm rs (RS.rhss rs) where
+
+  
+filterTerm :: Ord f => URM f -> Term f v -> Term f v
+filterTerm _   (aterm -> Var v) =  var v
+filterTerm urm (aterm -> t1 :@ t2) = filterTerm urm t1 `app` filterTerm urm t2
+filterTerm urm (aterm -> Fun f ts) = fun f [ti | (i,ti) <- zip [0..] ts, i `elem` uargs]
+  where uargs = usable urm f
+filterTerm _ _ = error "Hoca.URM.filterTerm pattern match failure"
+
+instance PP.Pretty f => PP.Pretty (URM f) where
+  pretty (URM m)
+    | null l = PP.text "{}"
+    | otherwise = PP.vsep [ PP.text "mu" PP.<> PP.parens (PP.pretty f)
+                            PP.<+> PP.text "="
+                            PP.<+> ppSet [PP.int i | i <- us]
+                          | (f,us) <- l]
+    where
+      l = Map.toList m
+      ppSet = PP.encloseSep PP.lbrace PP.rbrace PP.comma
+  
